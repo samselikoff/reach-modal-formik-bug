ae70ef00a3eff3e34ba7ccc84b71cc3a4c401357
Sam Selikoff <sam.selikoff@gmail.com>
2019-11-30 12:53:16 -0500

    The bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "@reach/dialog/styles.css";
+import { Dialog } from "@reach/dialog";
+import { useFormik } from "formik";
 
-function App() {
+export default function App() {
+  let [openDialog, open] = React.useState(null);
+  let close = () => open(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Reach Dialog + Formik</h1>
+
+      <p>This one works</p>
+      <button onClick={() => open(1)}>Open</button>
+      {openDialog === 1 && (
+        <Dialog aria-label="Meal form" onDismiss={close}>
+          <Form />
+        </Dialog>
+      )}
+
+      <p>So does this one</p>
+      <button onClick={() => open(2)}>Open</button>
+      {openDialog === 2 && <FormDialogSeparateComponents close={close} />}
+
+      <p>But this one doesn't</p>
+      <button onClick={() => open(3)}>Open</button>
+      {openDialog === 3 && <FormDialogSameComponent close={close} />}
     </div>
   );
 }
 
-export default App;
+function Form() {
+  const formik = useFormik({
+    initialValues: {
+      item: "",
+      date: "2014-01-01"
+    }
+  });
+
+  return (
+    <>
+      <h2>New Meal</h2>
+
+      <form>
+        <input {...formik.getFieldProps("item")} placeholder="Item" />
+        <input {...formik.getFieldProps("date")} placeholder="Date" />
+
+        <div>
+          <button type="submit">Save it!</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+function FormDialogSeparateComponents({ close }) {
+  return (
+    <Dialog aria-label="Meal form" onDismiss={close}>
+      <Form />
+    </Dialog>
+  );
+}
+
+function FormDialogSameComponent({ close }) {
+  const formik = useFormik({
+    initialValues: {
+      item: "",
+      date: "2014-01-01"
+    }
+  });
+
+  return (
+    <Dialog aria-label="Meal form" onDismiss={close}>
+      <h2>New Meal</h2>
+
+      <form>
+        <input {...formik.getFieldProps("item")} placeholder="Item" />
+        <input {...formik.getFieldProps("date")} placeholder="Date" />
+
+        <div>
+          <button type="submit">Save it!</button>
+        </div>
+      </form>
+    </Dialog>
+  );
+}
